affa94dae2c3d4537c2d50da1ca878ed63b74a9e
Vadim Brikulya <im.master99@gmail.com>
2021-04-04 22:41:04 +0300

    UP

---
+++ src/js/task-01.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+const totalCategories = document.querySelectorAll(".item");
+console.log('Он базарит шо',`в списке ${totalCategories.length} категории.`);
+console.log('- сИрьезна??');
+console.log('- ЕсссесИнаа=)');
+const categoriesArray = [...totalCategories].map(categories => `Категория: ${categories.children[0].textContent}
+Количество элементов: ${categories.children[1].children.length}`).join("\n");
+console.log(categoriesArray);
+
